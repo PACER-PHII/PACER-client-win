a04f1c5bfefccfbc44b31e87993036d594a8b08e
Myung Choi <myungchoi@gmail.com>
2022-06-10 08:41:42 -0400

    removing unnecessary files and fix env_config file

--- env_config.bat
+++ env_config.bat
@@ -1,3 +1,3 @@
-set ELR_RECEIVER_HOST=localhost
-set PORT=8087
-set V2_FILE_TO_SEND=hl7v2msg_fhirpatient.txt
+setx ELR_RECEIVER_HOST localhost
+setx PORT 8087
+setx V2_FILE_TO_SEND hl7v2msg_fhirpatient.txt
